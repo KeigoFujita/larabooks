d75e4df62f8f1bed77c05157efe863e3f31db1fc
Keigo Victor Fujita <keigofujita19@gmail.com>
2020-08-21 02:08:23 +0800

    Created the relationships between models

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function authors()
+    {
+        return $this->hasMany(Author::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
